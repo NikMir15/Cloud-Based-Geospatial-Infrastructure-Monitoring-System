58d90a124a858add9e07966efc608c4ef525300f
Nikunj Mirajkar <nikunjmirajkar15@gmail.com>
2026-06-17 23:10:40 +0100

    Implemented FastAPI API with PostgreSQL/PostGIS integration

---
+++ backend/app/database.py
@@ -0,0 +1,13 @@
+from sqlalchemy import create_engine, text
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+DATABASE_URL = os.getenv("DATABASE_URL")
+
+engine = create_engine(DATABASE_URL)
+
+
+def get_connection():
+    return engine.connect()
--- backend/main.py
+++ backend/main.py
@@ -1,4 +1,6 @@
 from fastapi import FastAPI
+from sqlalchemy import text
+from app.database import get_connection
 
 app = FastAPI()
 
@@ -8,3 +10,49 @@ def home():
         "project": "Cloud-Based Geospatial Infrastructure Monitoring System",
         "status": "Running"
     }
+
+from fastapi import FastAPI
+
+app = FastAPI()
+
+@app.get("/")
+def home():
+    return {
+        "project": "Cloud-Based Geospatial Infrastructure Monitoring System",
+        "status": "Running"
+    }
+
+@app.get("/health")
+def health():
+    return {
+        "status": "healthy",
+        "database": "connected"
+    }
+
+@app.get("/locations")
+def get_locations():
+
+    conn = get_connection()
+
+    result = conn.execute(
+        text("""
+            SELECT
+                id,
+                name,
+                description
+            FROM infrastructure_points
+        """)
+    )
+
+    locations = []
+
+    for row in result:
+        locations.append({
+            "id": row.id,
+            "name": row.name,
+            "description": row.description
+        })
+
+    conn.close()
+
+    return locations
